a865f3878c2c512532ba2b7a70fc8a8b7d3a4634
DmmDGM <dm12332131mdgaming@gmail.com>
2025-10-18 12:22:36 -0400

    rewrote core

--- src/core2.ts
+++ src/core2.ts
@@ -3,59 +3,234 @@ import BunSqlite from "bun:sqlite";
 import { createCipheriv, createDecipheriv, createHash, randomBytes } from "node:crypto";
 
 // Creates database
-export const database = new BunSqlite("database.sqlite");
+export const database = new BunSqlite("database.sqlite", {
+    create: true,
+    strict: true
+});
+
+// Creates excepts
+export enum Except {
+    USER_ENTRY_HIT,
+    USER_ENTRY_MISS,
+    USER_NAME_INVALID,
+    USER_PASS_FAILED,
+    USER_PASS_INVALID,
+    TOKEN_ENTRY_HIT,
+    TOKEN_ENTRY_MISS,
+    PRIVILEGE_ENTRY_HIT,
+    PRIVILEGE_ENTRY_MISS,
+    PRIVILEGE_PAIR_FAILED
+}
 
 // Creates sudo
 export const sudo = randomBytes(32).toBase64();
 
 // Defines user methods
-export function createUser(name: string, pass: string): void {
-    // Checks parameters
-    if(!/^[a-zA-Z0-9_]{3,}$/.test(name)) throw new Error("Bad")
-    try {
-        // Checks username
+export async function createUser(name: string, pass: string): Promise<void> {
+    // Validates name
+    if(!/^[a-zA-Z0-9_]{3,}$/.test(name)) throw Except.USER_NAME_INVALID;
+    
+    // Hashes pass
+    if(pass.length < 6) throw Except.USER_PASS_INVALID;
+    const hash = await Bun.password.hash(pass);
 
+    // Spawns uuid
+    const uuid = Bun.randomUUIDv7();
+
+    // Writes commit
+    try {
+        database.prepare(`
+            INSERT INTO users VALUES ($name, $hash, $uuid);    
+        `).run({ hash, name, uuid });
     }
     catch {
-
+        throw Except.USER_ENTRY_HIT;
     }
 }
-export function renameUser(name: string, pass: string, rename: string): void {
+export async function verifyUser(name: string, pass: string): Promise<boolean> {
+    // Verifies pass
+    const user = database.query(`
+        SELECT hash FROM users WHERE name = $name;
+    `).get({ name }) as {
+        hash: string;
+    } | null;
+    if(user === null) throw Except.USER_ENTRY_MISS;
+    return await Bun.password.verify(pass, user.hash);
 
 }
-export function repassUser(name: string, pass: string, repass: string): void {
+export async function renameUser(name: string, pass: string, rename: string): Promise<void> {
+    // Verifies user
+    if(!await verifyUser(name, pass)) throw Except.USER_PASS_FAILED;
 
+    // Validates name
+    if(!/^[a-zA-Z0-9_]{3,}$/.test(rename)) throw Except.USER_NAME_INVALID;
+    
+    // Writes commit
+    try {
+        database.prepare(`
+            UPDATE users SET name = $rename WHERE name = $name;
+        `).run({ name, rename });
+    }
+    catch {
+        throw Except.USER_ENTRY_HIT;
+    }
 }
-export function deleteUser(name: string, pass: string): void {
+export async function repassUser(name: string, pass: string, repass: string): Promise<void> {
+    // Verifies user
+    if(!await verifyUser(name, pass)) throw Except.USER_PASS_FAILED;
 
+    // Hashes pass
+    if(repass.length < 6) throw Except.USER_PASS_INVALID;
+    const rehash = await Bun.password.hash(repass);
+    
+    // Writes commit
+    try {
+        database.prepare(`
+            UPDATE users SET hash = $rehash WHERE name = $name;
+        `).run({ name, rehash });
+    }
+    catch {
+        throw Except.USER_ENTRY_HIT;
+    }
+}
+export async function deleteUser(name: string, pass: string): Promise<void> {
+    // Verifies user
+    if(!await verifyUser(name, pass)) throw Except.USER_PASS_FAILED;
+
+    // Writes commit
+    try {
+        database.prepare(`
+            DELETE FROM users WHERE name = $name;
+        `).run({ name });
+    }
+    catch {
+        throw Except.USER_ENTRY_MISS;
+    }
 }
 
 // Defines token methods
-export function generateToken(name: string, pass: string): void {
+export async function generateToken(name: string, pass: string): Promise<void> {
+    // Verifies user
+    if(!await verifyUser(name, pass)) throw Except.USER_PASS_FAILED;
 
+    // Generates token
+    const code = randomBytes(32).toString("base64");
+    const sign = encryptToken(code, pass);
+    const mask = obfuscateToken(code);
+
+    // Writes commit
+    try {
+        database.prepare(`
+            INSERT INTO tokens VALUES ($mask, $sign, $name)
+                ON CONFLICT (name) DO UPDATE SET mask = $mask, sign = $sign WHERE name = $name;
+        `).run({ mask, name, sign });
+    }
+    catch {
+        throw Except.TOKEN_ENTRY_HIT;
+    }
 }
-export function retrieveToken(name: string, pass: string): string {
+export async function retrieveToken(name: string, pass: string): Promise<string> {
+    // Verifies user
+    if(!await verifyUser(name, pass)) throw Except.USER_PASS_FAILED;
 
+    // Reveals code
+    const token = database.query(`
+        SELECT sign FROM tokens WHERE name = $name;
+    `).get({ name }) as {
+        sign: string;
+    } | null;
+    if(token === null) throw Except.TOKEN_ENTRY_MISS;
+    return decryptToken(token.sign, pass);
 }
 export function encryptToken(code: string, pass: string): string {
-
+    // Creates cipher
+    const key = createHash("sha-256").update(Buffer.from(pass, "utf8")).digest("hex");
+    const vector = randomBytes(16).toString("hex");
+    const cipher = createCipheriv(
+        "aes-256-gcm",
+        Buffer.from(key, "hex"),
+        Buffer.from(vector, "hex")
+    );
+    
+    // Encrypts code
+    const warp = cipher.update(code, "base64", "hex") + cipher.final("hex");
+    const tag = cipher.getAuthTag().toString("hex");
+    return [ warp, vector, tag ].join(";");
 }
 export function decryptToken(sign: string, pass: string): string {
-
+    // Creates decipher
+    const [ warp, vector, tag ] = sign.split(";");
+    const key = createHash("sha-256").update(Buffer.from(pass, "utf8")).digest("hex");
+    const decipher = createDecipheriv(
+        "aes-256-gcm",
+        Buffer.from(key, "hex"),
+        Buffer.from(vector, "hex")
+    );
+    
+    // Decrypts code
+    decipher.setAuthTag(Buffer.from(tag, "hex"));
+    return decipher.update(warp, "hex", "base64") + decipher.final("base64");
 }
 export function obfuscateToken(code: string): string {
-
+    // Obfuscates code
+    return createHash("sha-256").update(Buffer.from(code, "base64")).digest("hex");
 }
 
 // Defines privilege methods
-export function allowPrivilege(code: string, auth: string, pkey: string, pval: string): void {
+export function allowPrivilege(code: string, pkey: string, pval: string, auth: string): void {
+    // Checks privilege
+    if(
+        auth !== sudo &&
+        checkPrivilege(code, "system-admin") !== "1" &&
+        checkPrivilege(code, "manage-privilege") !== "1"
+    ) throw Except.PRIVILEGE_PAIR_FAILED;
 
+    // Obfuscates code
+    const mask = obfuscateToken(code);
+
+    // Writes commit
+    try {
+        database.prepare(`
+            INSERT INTO privileges VALUES ($mask, $pkey, $pval)
+                ON CONFLICT (mask, pkey) DO UPDATE SET pval = $pval WHERE mask = $mask AND pkey = $pkey;
+        `).run({ mask, pkey, pval });
+    }
+    catch {
+        throw Except.PRIVILEGE_ENTRY_HIT;
+    }
 }
-export function checkPrivilege(code: string, auth: string, pkey: string): string {
+export function checkPrivilege(code: string, pkey: string): string | null {
+    // Obfuscates code
+    const mask = obfuscateToken(code);
 
+    // Checks privilege
+    const privilege = database.query(`
+        SELECT pval FROM privileges WHERE mask = $mask AND pkey = $pkey;
+    `).get({ mask, pkey }) as {
+        pval: string;
+    } | null;
+    return privilege === null ? null : privilege.pval;
 }
-export function denyPrivilege(code: string, auth: string, pkey: string): void {
+export function denyPrivilege(code: string, pkey: string, auth: string): void {
+    // Checks privilege
+    if(
+        auth !== sudo &&
+        checkPrivilege(code, "system-admin") !== "1" &&
+        checkPrivilege(code, "manage-privilege") !== "1"
+    ) throw Except.PRIVILEGE_PAIR_FAILED;
 
+    // Obfuscates code
+    const mask = obfuscateToken(code);
+
+    // Writes commit
+    try {
+        database.prepare(`
+            DELETE FROM privileges WHERE mask = $mask AND pkey = $pkey;
+        `).run({ mask, pkey });
+    }
+    catch {
+        throw Except.PRIVILEGE_ENTRY_MISS;
+    }
 }
 
 // Initializes database
@@ -68,6 +243,7 @@ database.run(`
     CREATE TABLE IF NOT EXISTS users (
         name TEXT NOT NULL UNIQUE,
         hash TEXT NOT NULL UNIQUE,
+        uuid TEXT NOT NULL UNIQUE,
         PRIMARY KEY (name)
     );
 
@@ -87,7 +263,7 @@ database.run(`
     CREATE TABLE IF NOT EXISTS privileges (
         mask TEXT NOT NULL,
         pkey TEXT NOT NULL,
-        pval TEXT NOT NULL
+        pval TEXT NOT NULL,
         FOREIGN KEY (mask)
             REFERENCES tokens (mask)
                 ON UPDATE CASCADE
--- src/test.ts
+++ src/test.ts
@@ -4,3 +4,19 @@
 // const code = await verifyUser(Bun.env.ROOT_NAME!, Bun.env.ROOT_PASS!);
 // console.log(code);
 // console.log(indexUUIDs())
+
+import * as core2 from "./core2";
+
+// await core2.createUser("dmmdgm", "yuhhhh");
+// await core2.renameUser("dmmdgm", "yuhhhh", "dmmdgm2");
+// await core2.renameUser("dmmdgm2", "yuhhhh", "dmmdgm");
+// await core2.repassUser("dmmdgm2", "yuhhhh", "yuhhhhh");
+// await core2.repassUser("dmmdgm2", "yuhhhhh", "yuhhhh");
+// await core2.deleteUser("dmmdgm", "yuhhhh");
+// await core2.generateToken("dmmdgm", "yuhhhh");
+const code = await core2.retrieveToken("dmmdgm", "yuhhhh");
+// console.log(code);
+// core2.allowPrivilege(code, "system-admin", "1", core2.sudo);
+// core2.allowPrivilege(code, "system-admin", "1", code);
+core2.denyPrivilege(code, "system-admin", core2.sudo);
+// core2.denyPrivilege(code, "system-admin", code);
